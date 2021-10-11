def1571e326d2f099da6c02f909380056ffbb866
Leo-de-Riv3r <leoguti182@gmail.com>
2021-10-11 11:58:35 -0300

    queda mostrar la tabla

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-let boton1 = document.querySelector("#botonTipoDato")
-boton1.addEventListener('click', verificarTipoDato)
-let nav = document.querySelector("nav")
+let boton1 = document.querySelector("#botonTipoDato");
+boton1.addEventListener("click", verificarTipoDato);
+let nav = document.querySelector("nav");
 
 let cantNums;
 let nums = [];
@@ -12,84 +12,93 @@ let maxNum;
 let divs = [];
 let possibleRows;
 
-function verificarTipoDato(){
+function verificarTipoDato() {
   let cualitativo = document.querySelector("#cualitativo").checked;
   let cuantitativo = document.querySelector("#cuantitativo").checked;
 
-  if (cuantitativo){
-    nav.innerHTML = `<h2>Ingrese cantidad de datos</h2>` + 
-    `<input type="number" id="cant" min="2">` + 
-    `<button id="aceptarCantNums">Aceptar</button>`
-    ;
-    document.querySelector("#aceptarCantNums").addEventListener("click", verificarCantNums)
-  } else{
-
+  if (cuantitativo) {
+    nav.innerHTML =
+      `<h2>Ingrese cantidad de datos</h2>` +
+      `<input type="number" id="cant" min="2">` +
+      `<button id="aceptarCantNums">Aceptar</button>`;
+    document
+      .querySelector("#aceptarCantNums")
+      .addEventListener("click", verificarCantNums);
+  } else if (cuantitativo && cualitativo == false) {
+    alert("Seleccione una opcion");
+  } else {
   }
 }
 
-function verificarCantNums(){
-  cantNums = document.querySelector("#cant").value
-  if(cantNums == "" || cantNums == 0){
+function verificarCantNums() {
+  cantNums = document.querySelector("#cant").value;
+  if (cantNums == "" || cantNums == 0) {
     alert("Ingrese un numero valido");
-  }else{
+  } else {
     verificarDatos();
   }
 }
 let i = 0;
-function verificarDatos(){
+function verificarDatos() {
   i = 0;
-  while(i < cantNums){
-    dato = prompt(`Ingrese dato numero ${i+1}`)
+  while (i < cantNums) {
+    dato = prompt(`Ingrese dato numero ${i + 1}`);
 
-    if(dato == ''){
-      alert("Ingrese un dato valido")
-    } else{
+    if (dato == "") {
+      alert("Ingrese un dato valido");
+    } else {
       nums.push(dato);
       i++;
     }
   }
-  findDivisor()
+  findDivisor();
 }
 
-function findDivisor(){
-  maxNum = nums.reduce((acc, num) => Math.max(acc,num))
+function findDivisor() {
+  maxNum = nums.reduce((acc, num) => Math.max(acc, num));
   //maxNum has the highest value, so we need to find numbers from 2 to 10 that are divisors of maxNum and put them inot an array
-  for(let i = 2; i < 11; i++){
-    if(maxNum % i == 0){
+  for (let i = 2; i < 11; i++) {
+    if (maxNum % i == 0) {
       divs.push(i);
     }
   }
   nav.innerHTML = `<h2>Seleccione cantidad de filas a mostrar</h2>`;
-  for(let i = 0; i < divs.length; i++){
-    nav.innerHTML +=  `<label>${divs[i]}</label>` + 
-    `<input type="radio" name="rows" id="row${i}">`;
+  for (let i = 0; i < divs.length; i++) {
+    nav.innerHTML +=
+      `<label>${divs[i]}</label>` +
+      `<input type="radio" name="rows" value=${divs[i]}><br>`;
   }
   nav.innerHTML += `<button>Aceptar</button>`;
 
   document.querySelector("button").addEventListener("click", setRows);
 }
 
-function setRows(){
+function setRows() {
   possibleRows = document.querySelectorAll("input");
-  console.log(possibleRows)
-  //ERROR: .filter is not a function
-  /////////////
-  ///////////////////
-  //////////////
-  let flag = 0;
-  possibleRows.forEach((item) => {
-    if(item.checked == true){
-      rows = item
-      flag = 1;
-    }
-  });
-
-  if(flag == 0){
-    alert("Seleccione una opcion")
+  let arr = Array.from(possibleRows);
+  arr = arr.filter(item => item.checked == true);
+  if(arr[0] == undefined){
+    alert("Seleccione una opción")
+  } else{
+    divisor = maxNum / arr[0].value
+    showTable()
   }
-  ////////////
-  ////////////////
-  /////////////
-  rows = chosenRow[0];
-  alert(`Usted selecciono ${rows} filas a mostrar`);
+}
+
+function showTable(){
+  nav.innerHTML = `<table class="table table-bordered">` +
+  `<thead>` + 
+  `<tr>` + 
+  `<th>Dato</th>` + 
+  `<th>Frecuencia Absoluta</th>` + 
+  `<th>Frecuencia Acumulada</th>` + 
+  `<th>Frecuencia Relativa</th>` + 
+  `<th>Frecuencia Relativa Acumulada</th>` + 
+  `<th>Dato</th>` +
+  `</tr>` + 
+  `</thead>` + 
+  `<tbody>` + 
+  `</tbody>` + 
+  `</table>`;
+  alert("eto e boca amigo")
 }
